4c0a1b942f7436569efe5c3c318ec82bfc202e85
tom99763 <69715105+tom99763@users.noreply.github.com>
2021-05-23 21:52:52 +0800

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -7,44 +7,49 @@ def image_color_distort(inputs):
     inputs = tf.image.random_saturation(inputs,lower = 0.5, upper= 1.5)
     return inputs
 
-
 def preprocess_train(data_path,batch_size):
 
-    ds_gt_img = tf.keras.preprocessing.image_dataset_from_directory(
-        directory=data_path+'/gt_img',
-        shuffle=False
+    ds_img = tf.keras.preprocessing.image_dataset_from_directory(
+        directory=data_path+'/img',
+        shuffle=False,
+        image_size=(128,128),
+        batch_size=1
     )
 
 
     ds_gt_C = tf.keras.preprocessing.image_dataset_from_directory(
         directory=data_path+'/gt_C',
-        shuffle=False
+        shuffle=False,
+        image_size=(128,128),
+        batch_size=1
     )
 
     ds_gt_R = tf.keras.preprocessing.image_dataset_from_directory(
         directory=data_path+'/gt_R',
-        shuffle=False
+        shuffle=False,
+        image_size=(128,128),
+        batch_size=1
     )
 
-    ds =tf.data.Dataset.zip((ds_gt_img,ds_gt_C,ds_gt_R)).batch(batch_size)
-    AUTOTUNE = tf.data.AUTOTUNE
-    ds=ds.cache().prefetch(buffer_size=AUTOTUNE)
+    ds =tf.data.Dataset.zip((ds_img,ds_gt_C,ds_gt_R)).batch(batch_size)
     return ds
 
 
 def preprocess_test(data_path,batch_size):
-    ds_gt_img = tf.keras.preprocessing.image_dataset_from_directory(
-        directory=data_path+'/gt_img',
-        shuffle=False
+    ds_img = tf.keras.preprocessing.image_dataset_from_directory(
+        directory=data_path+'/img',
+        shuffle=False,
+        image_size=(128,128),
+        batch_size=1
     )
 
 
     ds_gt_C = tf.keras.preprocessing.image_dataset_from_directory(
         directory=data_path+'/gt_C',
-        shuffle=False
+        shuffle=False,
+        image_size=(128,128),
+        batch_size=1
     )
 
-    ds =tf.data.Dataset.zip((ds_gt_img,ds_gt_C)).batch(batch_size)
-    AUTOTUNE = tf.data.AUTOTUNE
-    ds=ds.cache().prefetch(buffer_size=AUTOTUNE)
+    ds =tf.data.Dataset.zip((ds_img,ds_gt_C)).batch(batch_size)
     return ds
