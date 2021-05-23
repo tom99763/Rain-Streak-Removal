7bbb0987446f45600db55c7e132be4221218e3f2
tom99763 <69715105+tom99763@users.noreply.github.com>
2021-05-23 19:52:23 +0800

    Update networks.py

--- model/networks.py
+++ model/networks.py
@@ -7,7 +7,9 @@ Achieive pursuit problem : Y=X1+X2+noise --> observe Y , split Y into X1 and X2
 
 Main Property : the inherent correlation among the rain streaks within an image
 should be observably stronger than that between the rain streaks and the background 
---> rain streak is a stronger signal than rain streak with background, so we should find 'pure' rain streak
+--> rain streak is a stronger signal than rain streak with background
+
+-->so we tend to use model to find 'easy' rain streak rather than 'complex' background 
 
 First Stage : rain streak component (denoted by R1) is initially generated
 
