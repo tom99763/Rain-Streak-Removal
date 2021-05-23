f34e118c7fbb2004111fe30bef676c196d242f50
tom99763 <69715105+tom99763@users.noreply.github.com>
2021-05-23 21:16:46 +0800

    Add files via upload

---
+++ model/loss_function.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+
+'''labels、loss
+gt:clear_img 、 pure_rain_streak
+pred:R1、R2、desired_clear_img
+loss=loss_hybrid_R1+loss_hybrid_R2+loss_hybrid_desired
+'''
+
+l1 = tf.keras.losses.MeanAbsoluteError()
+def hybrid_loss(gen,gt):
+    l1_loss=l1(gt,gen) #l1 distance of two image
+    ssim_loss=-tf.reduce_mean(tf.image.ssim(gen,gt,1.0)) #structure similarity of two image, sum over batch
+    loss=l1_loss+ssim_loss
+    return loss
