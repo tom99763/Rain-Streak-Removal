64bf0c516afe4ff7c9a55512d47f2c24a1557f91
tom99763 <69715105+tom99763@users.noreply.github.com>
2021-05-30 08:22:50 +0800

    Update networks.py

--- model/networks.py
+++ model/networks.py
@@ -1,15 +1,12 @@
 import tensorflow as tf
-from Blocks import RDB,SDAB,MAM
+from model.Blocks import RDB,SDAB,MAM
 
 '''Model Design
 
 Achieive pursuit problem : Y=X1+X2+noise --> observe Y , split Y into X1 and X2  
 
-Main Property : the inherent correlation among the rain streaks within an image
-should be observably stronger than that between the rain streaks and the background 
---> rain streak is a stronger signal than rain streak with background
-
--->so we tend to use model to find 'easy' rain streak rather than 'complex' background 
+Main Property : the inherent correlation among the rain steaks within an image
+should be observably stronger than that between the rain streaks and the background
 
 First Stage : rain streak component (denoted by R1) is initially generated
 
@@ -18,9 +15,7 @@ Second Stage : The main purpose of integrating the initial rain component
 stage of locating possible rain pixels while suppressing
 non-rain pixels in the image
 
-why 2 stages?:不要被同個石頭絆倒兩次,同個腦子再給你看一次一定要學乖!
-
-Why element-wise addition operation (Conv + ReLU add stage2) ? : The main advantage
+Why element-wise addition operation (Conv + ReLU) ? : The main advantage
 of this element-wise addition operation is to enhance the
 shallow layer features for the second stage of the proposed
 deep learning model to learn the distribution of rain streaks.
@@ -37,15 +32,20 @@ class SSDRNet(tf.keras.Model):
             kernel_size=3,
             strides=1,
             padding='same',
-            activation='relu'
+            activation=None,
         )
+
+        self.act1=tf.keras.layers.PReLU(tf.constant_initializer(0.25))
         self.conv_second_stage=tf.keras.layers.Conv2D(
             filters=32,
             kernel_size=3,
             strides=1,
             padding='same',
-            activation='relu'
+            activation=None,
         )
+        self.act2=tf.keras.layers.PReLU(tf.constant_initializer(0.25))
+
+
 
         self.conv_output = tf.keras.layers.Conv2D(
             filters=3,
@@ -55,10 +55,10 @@ class SSDRNet(tf.keras.Model):
             activation=None
         )
 
-        self.feature_extractor1=tf.keras.Sequential([RDB() for _ in range(3)]) #extract representative feature
-        self.refinement=tf.keras.Sequential([SDAB() for _ in range(4)]) #refine relationship between pixel and pixel, pay attention on probable rain streak pattern
-        self.feature_extractor2=tf.keras.Sequential([RDB() for _ in range(2)]) #extract representative feature
-        self.transformer=MAM() #transform feature by multi-scale dilated convolution method to represent richer 
+        self.feature_extractor1=tf.keras.Sequential([RDB() for _ in range(3)])
+        self.refinement=tf.keras.Sequential([SDAB() for _ in range(4)])
+        self.feature_extractor2=tf.keras.Sequential([RDB() for _ in range(2)])
+        self.transformer=MAM()
 
         self.concat = tf.keras.layers.Concatenate(axis=-1)
 
@@ -68,26 +68,26 @@ class SSDRNet(tf.keras.Model):
         '''
 
         #first stage
-        x1=self.conv_first_stage(x)
+        x1=self.act1(self.conv_first_stage(x))
         F1=self.feature_extractor1(x1,training=training)
-        F1=self.refinement(F1,training=training) 
+        F1=self.refinement(F1,training=training)
         F1=self.feature_extractor2(F1,training=training)
         R1=self.transformer(F1,training=training)
 
 
         #second stage
         H=self.concat([x,R1])
-        x2=self.conv_second_stage(H)
+        x2=self.act2(self.conv_second_stage(H))
         F2=tf.add(x1,x2)
         F2=self.feature_extractor1(F2,training=training)
-        F2=self.refinement(F2,training=training) 
+        F2=self.refinement(F2,training=training)
         F2=self.feature_extractor2(F2,training=training)
         R2=self.transformer(F2,training=training) #R2=C1 or C2
 
 
         #X=Component1+Component2+noise , get desired C=C1 or C2
         C_with_noise=tf.subtract(x,R2) #C+noise=X-R2
-        denoise=self.conv_output(C_with_noise) #denoise=-noise=conv(C+noise)-->find a feature s.t. denoised noise in C_with_noise
+        denoise=self.conv_output(C_with_noise) #denoise=-noise=conv(C+noise)
         C=tf.add(C_with_noise,denoise) #C+noise+denoise=C=desired Component
         return R1,R2,C
 
